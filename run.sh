cf528fe73cf6b96c1c17b89270aa57e3ca9c3112
Adri?n Deccico <deccico@gmail.com>
2011-12-28 21:57:55 +0000

    adding runner script

--- run.sh
+++ run.sh
@@ -2,11 +2,17 @@
 set -e
 LOGFILE=/var/log/gunicorn/django_app.log
 LOGDIR=$(dirname $LOGFILE)
-NUM_WORKERS=3
+NUM_WORKERS=3  #recommended formula here is 1 + 2 * NUM_CORES
+ 
+#we don't want to run this as root..
+USER=www-data
+GROUP=www-data
+ 
 cd /home/ubuntu/django_app
 source bin/activate
 cd app
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec bin/gunicorn_django -w $NUM_WORKERS \
+exec gunicorn_django -w $NUM_WORKERS \
   --log-level=debug \
-  --log-file=$LOGFILE 2>>$LOGFILE
+  --log-file=$LOGFILE 2>>$LOGFILE \
+  --user=$USER --group=$GROUP
